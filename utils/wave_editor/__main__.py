cf11ab7f3b236276f904eb52aaa5a4b246ab8f55
Tim Savage <tim@savage.company>
2017-03-07 09:38:55 +1100

    Updates to setup editor panel

--- utils/wave_editor/__main__.py
+++ utils/wave_editor/__main__.py
@@ -3,9 +3,17 @@ import sys
 
 from PySide.QtGui import *
 
-from wave_editor._ui import Ui_MainWindow, Ui_editorPanel
+from wave_editor._ui.main_window import Ui_MainWindow
+from wave_editor._ui.wave_editor import Ui_EditorWindow
 
 app = QApplication(sys.argv)
+
+
+class EditorPanel(QMdiSubWindow):
+    def __init__(self, parent):
+        super(EditorPanel, self).__init__(parent)
+        self.ui = Ui_EditorWindow()
+        self.ui.setupUi(self)
 
 
 class MainWindow(QMainWindow):
@@ -15,6 +23,12 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
         self.ui.action_Quit.triggered.connect(self.action_Quit__triggered)
+
+        self._editors = []
+        editor = EditorPanel(self)
+        self._editors.append(editor)
+        self.ui.mdiArea.addSubWindow(editor)
+        editor.show()
 
     def action_Quit__triggered(self):
         app.exit()
