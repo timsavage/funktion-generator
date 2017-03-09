170fc0b84401f933220f93200a842c1ddf61eacd
Tim Savage <tim@savage.company>
2017-03-09 16:08:27 +1100

    Updated wave functions to be generators and modifiers

--- utils/wave-editor/wave_editor/wave_functions.py
+++ utils/wave-editor/wave_editor/wave_functions.py
@@ -3,61 +3,72 @@ from __future__ import division, print_function
 import math
 
 DYNAMIC_RANGE = 0xFF
-WAVE_LENGTH = 0xFF
+WAVE_LENGTH = 256
 
-# The useful circle constant!
-TAU = math.pi * 2
-
-
-class WaveGenerator(object):
-    def __init__(self, function):
-        """
-        Initialise function generator
-
-        :param function: Callable function to generate a waveform.
-
-        """
-        self.function = function
-        self.dynamic_range = DYNAMIC_RANGE
-        self.wave_length = WAVE_LENGTH
-
-    def __iter__(self):
-        args = (self.dynamic_range, self.wave_length)
-        for x in range(0, self.wave_length + 1):
-            yield self.function(x, *args)
+TAU = math.pi * 2  # The useful circle constant!
 
 
-def sine_wave_function(x, dynamic, _):
-    origin = dynamic >> 1
-    return origin + int(math.sin((x / float(dynamic)) * TAU) * origin + 0.5)
+def zero_wave():
+    """
+    A flat line about the origin.
+    """
+    origin = DYNAMIC_RANGE >> 1
+    return [origin for _ in range(WAVE_LENGTH)]
 
 
-def square_wave_function(x, dynamic, length):
-    half_wave = length >> 1
-    return dynamic if x <= half_wave else 0
+def sine_wave():
+    """
+    Generate a sine wave.
+    """
+    origin = DYNAMIC_RANGE >> 1
+    f_dynamic = float(DYNAMIC_RANGE)
+    return [origin + int(math.sin((x / f_dynamic) * TAU) * origin + 0.5) for x in range(WAVE_LENGTH)]
 
 
-def saw_tooth_function(x, dynamic, _):
-    origin = dynamic >> 1
-    return (origin + x) % dynamic
+def square_wave():
+    """
+    Generate a square wave.
+    """
+    half_wave = WAVE_LENGTH >> 1
+    high = DYNAMIC_RANGE,
+    low = 0
+    return [high if x < half_wave else low for x in range(WAVE_LENGTH)]
 
 
-def reverse_saw_tooth_function(x, dynamic, _):
-    origin = dynamic >> 1
-    return (origin - x) % dynamic
-
-
-def triangle_wave_function(x, dynamic, length):
-    origin = dynamic >> 1
-    half_wave = length >> 1
-    quarter_wave = length >> 1
+def triangle_wave():
+    """
+    Generate a triangle wave.
+    """
+    origin = DYNAMIC_RANGE >> 1
+    half_wave = WAVE_LENGTH >> 1
+    quarter_wave = half_wave >> 1
     three_quarter_wave = half_wave + quarter_wave
-    if 0 <= x < quarter_wave:
-        return origin + x * 2
-    elif quarter_wave <= x < three_quarter_wave:
-        return dynamic - (x - quarter_wave) * 2
-    else:
-        return ((x - three_quarter_wave) * 2) - 1
+
+    def wave_func(x):
+        if 0 <= x < quarter_wave:
+            return origin + x * 2
+        elif quarter_wave <= x < three_quarter_wave:
+            return DYNAMIC_RANGE - (x - quarter_wave) * 2
+        else:
+            return ((x - three_quarter_wave) * 2) - 1
+
+    return [wave_func(x) for x in range(WAVE_LENGTH)]
+
+
+def sawtooth_wave():
+    """
+    Generate a sawtooth wave.
+    """
+    origin = DYNAMIC_RANGE >> 1
+    return [(origin + x) % DYNAMIC_RANGE for x in range(WAVE_LENGTH)]
+
+
+def reverse_sawtooth_wave():
+    """
+    Generate a reverse sawtooth wave.
+    """
+    origin = DYNAMIC_RANGE >> 1
+    return [(origin - x) % DYNAMIC_RANGE for x in range(WAVE_LENGTH)]
 
 
 def invert_wave(wave):
@@ -72,14 +83,3 @@ def mirror_wave(wave):
     Invert the wave about half a wave length
     """
     return list(reversed(wave))
-
-
-if __name__ == '__main__':
-    wave = list(WaveGenerator(saw_tooth_function))
-    print(wave)
-    from collections import Counter
-
-    wave_counter = Counter(wave)
-    print("Summary:", wave_counter)
-    print("Most Common:", wave_counter.most_common())
-    print("Length:", sum(wave_counter.values()))
