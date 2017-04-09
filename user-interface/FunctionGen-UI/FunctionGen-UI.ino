539be03b34692fc1374c16805c6e740c942a41e9
Tim Savage <tim@savage.company>
2017-04-10 01:14:02 +1000

    More work on the UI to display current state.\nInitilisation process is roughed out some refactors may be required to decouple parts of the code

--- user-interface/FunctionGen-UI/FunctionGen-UI.ino
+++ user-interface/FunctionGen-UI/FunctionGen-UI.ino
@@ -6,29 +6,22 @@
 #include <Wire.h>
 
 #include "wave_tables.h"
-
-// Color definitions
-#define BLACK    0x0000
-#define BLUE     0x001F
-#define RED      0xF800
-#define GREEN    0x07E0
-#define CYAN     0x07FF
-#define MAGENTA  0xF81F
-#define YELLOW   0xFFE0 
-#define WHITE    0xFFFF
+#include "ui.h"
 
 #define __TFT_CS  D0  //(D0)
 #define __TFT_DC  D8   //(D1)
 
 
-const char* ssid = "Penguins!";
-const char* password = "";
+
 const char* mdnsName = "FunktionGen";
 
 
 TFT_ILI9163C tft = TFT_ILI9163C(__TFT_CS, __TFT_DC);
+WiFiUDP server = WiFiUDP();
 WaveTable currentWave = WaveTable();
+
 uint32_t currentFrequency = 1000; // 1kHz
+
 
 /**
  * Draw a wave table
@@ -54,102 +47,119 @@ void drawWave(TFT_ILI9163C* display, const WaveTable* waveTable, uint8_t scale=0
   }
 }
 
-void init_halt(const char* message) {
-    tft.setTextColor(RED);
-    tft.print("[!] Halt: "); tft.println(message);
-    while(1) { delay(1000); }
+
+void init_message(const char* message, uint16_t color=WHITE, const char icon='+') {
+  tft.setTextColor(color);
+  tft.print("["); tft.print(icon); tft.print("] "); tft.print(message);
+  Serial.print("["); Serial.print(icon); Serial.print("] "); Serial.print(message);
 }
 
-void init_network() {
-  tft.println("[+] Init Network");
-  
-  tft.print("[-] Joining SSID: "); tft.println(ssid);
+#define INIT_SYSTEM(message) init_message(message)
+#define INIT_SUBSYSTEM(message) init_message(message, LIGHT_GREY, '-')
+#define INIT_INFO(message) init_message(message, CYAN, '?')
+#define INIT_ERROR(message) init_message(message, RED, '!')
+#define INIT_HALT(message) init_message(message, RED, '!'); while(1) { delay(1000); }
+#define INIT_OK(value) tft.setTextColor(GREEN); tft.print(value); Serial.print(value)
+#define INIT_PRINT(value) tft.print(value); Serial.print(value)
+
+
+/**
+ * Initilise network
+ * 
+ * @param connect_timeout - Timeout for wifi connection (in seconds)
+ */
+void init_network(uint16_t port=10001, uint8_t connect_timeout=20) {
+  INIT_SYSTEM("Init Network\n");
+
+  // Wifi
+  INIT_SUBSYSTEM("Joining SSID: "); INIT_PRINT(ssid); INIT_PRINT('\n');
   WiFi.begin(ssid, password);
-  
-  tft.print("[-] Connecting ");
+
+  INIT_SUBSYSTEM("Connecting ");
+  connect_timeout = connect_timeout * 4; // Loop is ~1/4 second
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500); tft.print(".");
+    if (connect_timeout-- == 0) {
+      INIT_ERROR("\nConnect timeout\n");
+      return;
+    }
+    delay(250); 
+    INIT_PRINT(".");
   }
-  tft.println("");
+  INIT_PRINT('\n'); INIT_INFO("IP address: "); INIT_PRINT(WiFi.localIP()); INIT_PRINT('\n');
 
-  tft.print("[-] IP address: "); tft.println(WiFi.localIP());
-
-
-  tft.println("[+] Starting MDNS");
-  
-  tft.print("[-] Listening on: "); tft.println(mdnsName);  
+  // Zeroconf
+  INIT_SYSTEM("Starting mDNS service\n");
+  INIT_SUBSYSTEM("Listening on: "); INIT_PRINT(mdnsName); INIT_PRINT('\n'); 
   if (!MDNS.begin(mdnsName)) {
-    init_halt("Error setting up MDNS responder!");
+    INIT_ERROR("Unable to start mDNS service\n");
+    return;
   }
-  tft.println("[-] Adding service: wave");
-  MDNS.addService("wave", "tcp", 10001);
+
+  INIT_SYSTEM("Start UDP server\n");
+  if (!server.begin(port)) {
+    INIT_ERROR("Unable to start UDP server\n");
+    return;
+  }
+
+  INIT_SUBSYSTEM("Publish mDNS service\n");
+  MDNS.addService("wave", "udp", port);
 }
 
+/**
+ * Initialise
+ */
 void initialise() {
   // Initilaise tft first.
   tft.begin();
-  tft.setTextColor(WHITE);
 
   // Initialise File system
-  tft.println("[+] Init FS");
+  INIT_SYSTEM("Init FS\n");
   SPIFFS.begin();
   FSInfo fs_info;
   if (SPIFFS.info(fs_info)) {
-    tft.print("[-] Total Bytes: "); tft.println(fs_info.totalBytes);
-    tft.print("[-] Used Bytes: "); tft.println(fs_info.usedBytes);
+    INIT_INFO("Total: "); INIT_PRINT(fs_info.totalBytes / 1024); INIT_PRINT("kB\n");
+    INIT_INFO("Total: "); INIT_PRINT(fs_info.usedBytes / 1024); INIT_PRINT("kB\n");
   } else {
-    tft.setTextColor(RED);
-    tft.print("[!] Failed");
-    tft.setTextColor(WHITE);
+    INIT_HALT("Failed initilising filesystem.");
   }
 
   // Init TWI
-  tft.println("[+] Init TWI");
+  INIT_SYSTEM("Init TWI\n");
   Wire.begin();
+ 
+  // Init DDS Generator
+  INIT_SYSTEM("Init DDS Generator\n");
 
   // Init network
   init_network();
-  
-  // Init DDS Generator
-  tft.println("[+] Init DDS Generator");
+
+  // Complete
+  INIT_OK("Init complete.");  
 }
+
 
 void setup() {
+  Serial.begin(115200);
+  
   initialise();
-
-  tft.setTextColor(YELLOW);
-  Dir dir = SPIFFS.openDir("/");
-  while (dir.next()) {
-    String fileName = dir.fileName();
-    if (fileName.endsWith(".wave")) {
-      tft.print(fileName); tft.print(" (");
-      File f = dir.openFile("r");
-      tft.print(f.size()); tft.println("b)");
-    }
-  }
-
-  WaveTable wave;
+  delay(500);
   
-  wt_zero(&wave);
-  uint8_t result = wt_load(&wave, "/Random.wave");
-  if (result != WAVE_FILE_OK) {
-    tft.setTextColor(RED);
-    tft.print("Error reading wave: "); tft.println(wt_errorMessage(result));
-    return;
-  } else {
-    tft.setTextColor(GREEN);
-    tft.print("Read wave: "); tft.println(wave.name);
-  }
-  
-  drawWave(&tft, &wave, 1, 0, 0, CYAN);
-  wt_mirror(&wave);
-  drawWave(&tft, &wave, 1, 0, 0, MAGENTA);
-  wt_invert(&wave);
-  drawWave(&tft, &wave, 1, 0, 0, BLUE);
+  tft.clearScreen();
+  ui_update(&tft);
 }
 
+
+char packetBuffer[0xFF];
+
 void loop(void) {
-  
+  size_t packetSize = server.parsePacket();
+  if (packetSize) {
+    // Process UDP packet.
+    size_t len = server.read(packetBuffer, 0xFF);
+    if (len > 0) {
+      Serial.print("Received: "); Serial.println(packetBuffer);
+    }
+  }
 }
 
 
