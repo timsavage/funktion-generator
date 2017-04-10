6324b623a7a188248a0212beb5cd3821ebb84fce
Tim Savage <tim@savage.company>
2017-04-10 22:07:10 +1000

    Added initial device and discovery code

---
+++ utils/wave-editor/wave_editor/devices/discovery.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import, unicode_literals
+
+import logging
+
+from PySide import QtCore
+from zeroconf import ServiceBrowser, Zeroconf
+
+logger = logging.getLogger(__name__)
+zeroconf = Zeroconf()
+
+
+class Devices(QtCore.QObject):
+    """
+    Listener object that listens for new Funktion Gen devices
+    """
+    serviceAdd = QtCore.Signal((str, ))
+    serviceRemove = QtCore.Signal((str, ))
+
+    def __init__(self):
+        super(Devices, self).__init__()
+
+        self.browser = ServiceBrowser(zeroconf, "function-gen._udp.local", self)
+        self._devices = []
+
+    def _populate(self):
+        pass
+
+    def remove_service(self, zeroconf, type_, name):
+        logger.info("Service %s removed", name)
+        self.serviceRemove.emit(name)
+
+    def add_service(self, zeroconf, type_, name):
+        info = zeroconf.get_service_info(type_, name)
+        logger.info("Service %s added, service info: %s", name, info)
+        self.serviceAdd.emit(name)
+
